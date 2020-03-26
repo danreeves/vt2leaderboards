bf7a5b64c51f1bee72af11aa59122a30bf62cd41
Dan Reeves <hey@danreev.es>
2020-03-26 19:06:40 +0000

    add server

---
+++ server.js
@@ -0,0 +1,38 @@
+var bankai = require("bankai/http");
+var http = require("http");
+var path = require("path");
+var merry = require("merry");
+
+var compiler = bankai(path.join(__dirname, "index.js"));
+var app = merry();
+
+app.route("GET", "/api", function (req, res, ctx) {
+  res.writeHead(200, { "Content-Type": "application/json" });
+  res.write(JSON.stringify({ hello: "world" }));
+  res.end();
+});
+
+app.route("default", function (req, res, ctx) {
+  compiler(req, res, function () {
+    res.statusCode = 404;
+    res.end("not found");
+  });
+});
+
+app.listen(process.env.PORT || 8080);
+
+/*
+fetch("https://5107.playfabapi.com/Client/GetLeaderboard", {
+  method: "POST",
+  body: JSON.stringify({
+    MaxResultsCount: 100,
+    StatisticName: "s2_weave_score_1_players",
+    StartPosition: 0,
+    ProfileConstraints: { ShowLinkedAccounts: true }
+  }),
+  headers: {
+    "X-Authorization": "",
+    "Content-Type": "application/json"
+  }
+});
+*/
