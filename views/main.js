e9afab954e59df449eb31ea2263cac89625fbbd1
Dan Reeves <hey@danreev.es>
2020-03-27 04:33:41 +0000

    style list

--- views/main.js
+++ views/main.js
@@ -92,25 +92,66 @@ function loading() {
 }
 
 function teamMember(data) {
-  return html`
-    <div>
-      <div>${data.username}</div>
-      <div>${data.tier}</div>
-      <div>${data.score}</div>
-    </div>
-  `;
+  return html`<div>${data.username}</div>`;
 }
 
+var teambox = css`
+  :host {
+    width: 500px;
+  }
+`;
+
+var scoreRow = css`
+  :host {
+    display: flex;
+    flex-direction: row;
+    justify-content: space-around;
+    margin-bottom: 1.5em;
+  }
+  :host > div {
+    display: flex;
+    flex-basis: 20%;
+  }
+  :host > .rank {
+    flex-basis: 15%;
+  }
+  :host > .name {
+    flex-basis: 65%;
+  }
+`;
+
+var teamCol = css`
+  :host {
+    flex-direction: column;
+  }
+`;
+
 function team(team, i) {
-  return html`<li>
-    <div>${i + 1}</div>
-    ${team.map((item) => teamMember(item))}
+  const firstMember = team[0];
+  const { tier, score } = firstMember;
+  return html`<li class="flex flex-column ${teambox}">
+    <div class=${scoreRow}>
+      <div class="rank">${i + 1}</div>
+      <div class="name ${teamCol}">
+        ${team.map((item) => teamMember(item))}
+      </div>
+      <div>${tier}</div>
+      <div>${score}</div>
+    </div>
   </li>`;
 }
 
 function list(teams) {
+  var firstTeam = teams[0];
+  var numPlayers = firstTeam.length;
   return html`
-    <ul class="list pl0 measure center">
+    <ul class="list pl0 measure center flex flex-column">
+      <div class="fw6 ${scoreRow} ${teambox}">
+        <div class="rank">Rank</div>
+        <div class="name">${numPlayers === 1 ? "Player" : "Team"}</div>
+        <div>Tier</div>
+        <div>Score</div>
+      </div>
       ${teams.map((item, i) => team(item, i))}
     </ul>
   `;
